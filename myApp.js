deca2044f1fb573e5aeeb94532e8ae837ed22798
firstChairCoder <md1qq7@gmail.com>
2023-11-21 18:05:24 +0100

    chore: modify findOneByFood
    This is done using the .findOne() method

--- myApp.js
+++ myApp.js
@@ -64,7 +64,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findPersonById = (personId, done) => {
