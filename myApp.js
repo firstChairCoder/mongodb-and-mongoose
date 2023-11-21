b4eb1d602387d1822d08423ff1882f7143573925
firstChairCoder <md1qq7@gmail.com>
2023-11-21 18:07:37 +0100

    chore: modify findPersonById
    This is done using the .findById() method

--- myApp.js
+++ myApp.js
@@ -74,7 +74,13 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findEditThenSave = (personId, done) => {
