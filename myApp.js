d104725c356a1449f6822a48d34b48bd5509ee75
firstChairCoder <md1qq7@gmail.com>
2023-11-21 16:41:58 +0100

    chore: initial commit
    This includes completion of the personSchema model assignment

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+let mongoose = require("mongoose");
 
 
-let Person;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let personSchema = new mongoose.Schema({
+  name: {type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+let Person = mongoose.model('Person', personSchema);
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
